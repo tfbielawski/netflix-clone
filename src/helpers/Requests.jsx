11702b4ba9b3c9b92c6bba6b1a4ae277003cc4ac
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-20 17:14:31 -0500

    Added axios and requests helper files for calling api. Implemented api call with useEffect hook

---
+++ src/helpers/Requests.jsx
@@ -0,0 +1,15 @@
+/* Best Practice is to hide the key in a .env file, but for demo purposes I'm leaving it here */
+export const API_KEY ="f5622499313c2dad7c702c804a4c478d";
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    // fetchHororMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
