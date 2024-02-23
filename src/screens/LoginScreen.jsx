7d73cea1ceb59f08f6fc43e3cef0934fe3b062a8
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-23 17:38:55 -0500

    Building login screen component

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/LoginScreen.css";
+
+function LoginScreen() {
+    return (
+        <div className="loginScreen">
+            <div className="loginScreen_background">
+                <img src />
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
