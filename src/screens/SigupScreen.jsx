0d424dd27362936fe1726b0849427944250d960c
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-24 14:13:30 -0500

    signup screen complete

--- src/screens/SigupScreen.jsx
+++ src/screens/SigupScreen.jsx
@@ -3,11 +3,17 @@ import "../styles/SignupScreen.css"
 
 function SigupScreen() {
     return (
-        <div className="signupScreen">SigupScreen
-            <h1> Sign In</h1>
-            <input placeholder="Email" type="email"/>
-            <input placeholder="Password" type="password"/>
-            <h4></h4>
+        <div className="signupScreen">
+            <form>
+                <h1> Sign In</h1>
+                <input placeholder="Email" type="email"/>
+                <input placeholder="Password" type="password"/>
+                <button type="submit">Sign Up</button>
+                <h4>
+                    <span className="signupScreen_gray">New to Netflix? </span> 
+                    <span className="signupScreen_link">Sign up now!</span>
+                </h4>
+            </form>
         </div>
     )
 }
