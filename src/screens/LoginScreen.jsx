956583d1c3adfc22511f40860a0a61045bd173ce
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-24 13:05:12 -0500

    Adding image assets

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "../styles/LoginScreen.css";
+import NET3 from "../assets/net3.png";
 
 function LoginScreen() {
     return (
         <div className="loginScreen">
             <div className="loginScreen_background">
-                <img src />
+                <img 
+                    className="loginScreen_logo"
+                    src={NET3} 
+                    alt="Netflix Logo" 
+                />
+                <button 
+                    className="loginScreen_button">SIGN IN
+                </button>
+                <div className="loginScreen_gradient" />
+                <div className="loginScreen_body">
+                   <>
+                    <h1>Unlimited Films, TV programs, and more.</h1>
+                   </> 
+                </div>
             </div>
         </div>
     )
