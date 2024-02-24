cf8f55496b8f90d8d7be8f8d8b757d860935a037
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-24 13:39:18 -0500

    styled login screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/LoginScreen.css";
 import NET3 from "../assets/net3.png";
 
 function LoginScreen() {
+    const [signIn, setSignIn] =  useState(false);
+
     return (
         <div className="loginScreen">
             <div className="loginScreen_background">
@@ -11,15 +13,37 @@ function LoginScreen() {
                     src={NET3} 
                     alt="Netflix Logo" 
                 />
-                <button 
-                    className="loginScreen_button">SIGN IN
+                <button
+                    className="loginScreen_button"
+                    onClick={() => setSignIn(true)} 
+                >SIGN IN                 
                 </button>
                 <div className="loginScreen_gradient" />
-                <div className="loginScreen_body">
-                   <>
+            </div>
+            <div className="loginScreen_body">
+                {SignUpScreen ? (
+                    <SignIn />
+                ) : (
+                    <>
                     <h1>Unlimited Films, TV programs, and more.</h1>
-                   </> 
-                </div>
+                    <h2>Watch anywhere. Cancel at any time.</h2>
+                    <h3>Ready to watch? Enter your email 
+                        to create or restart your membership</h3>
+
+                    <div className="loginScreen_input">
+                        <form>
+                            <input type="email" placeholder="email address" />
+                            <button 
+                                className="loginScreen_getStarted"
+                                onClick={() => setSignIn(true)}
+                            >
+                                GET STARTED
+                            </button>
+                        </form>
+                    </div>   
+                </>
+                )
+            }    
             </div>
         </div>
     )
