c4ea134d0dd69e0365c30e64d2b3a1098782e024
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-25 16:25:07 -0500

    added profile screen

---
+++ src/screens/ProfileScreen.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect} from "react";
+import "../styles/ProfileScreen.css";
+
+function ProfileScreen() {
+    return (
+        <div>
+            PROFILE PROFILE PROFILE
+        </div>
+    )
+}
+
+export default ProfileScreen
