8f4224db7456cb504a6fc72e428e8a3c1eb58d13
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-25 12:59:34 -0500

    authentication working

--- src/screens/SigupScreen.jsx
+++ src/screens/SigupScreen.jsx
@@ -1,24 +1,60 @@
-import React from "react";
-import "../styles/SignupScreen.css"
+import React, {useRef} from "react";
+import { auth } from "../helpers/firebase.jsx";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"; //imported to resolve errors ( createUserWithEmailAndPassword )
+import "../styles/SignupScreen.css";
 
 function SigupScreen() {
 
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    //Create an account with user/pw
     const register = (e) => {
         // Prevent default refreshing form on click
         e.preventDefault();
+        createUserWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((authUser) => {
+            console.log("LOOk 1--->", authUser)
+        })
+        .catch((err) => {
+            alert(err.message);
+        });
     };
 
     const signIn = (e) =>{
         // Prevent default refreshing form on click
-        e.preventDefault();
+        e.preventDefault(); 
+        signInWithEmailAndPassword(
+            auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((authUser) => {
+            console.log("LOOk 2--->", authUser)
+        })
+        .catch((err) => {
+            alert(err.message);
+        });
     }
 
     return (
         <div className="signupScreen">
             <form>
                 <h1> Sign In</h1>
-                <input placeholder="Email" type="email"/>
-                <input placeholder="Password" type="password"/>
+                <input 
+                    ref={emailRef} 
+                    placeholder="Email" 
+                    type="email"
+                />
+                <input 
+                    ref={passwordRef} 
+                    placeholder="Password" 
+                    type="password"
+                />
                 <button type="submit" onClick={signIn}>Sign In</button>
                 <h4>
                     <span className="signupScreen_gray">New to Netflix? </span> 
