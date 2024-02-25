afa313da4c2bf6be3bf9a53cb5ff2895b5fc3117
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-25 11:29:46 -0500

    Added event handlers to prevent page refresh on click

--- src/screens/SigupScreen.jsx
+++ src/screens/SigupScreen.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import "../styles/SignupScreen.css"
 
 function SigupScreen() {
+
+    const register = (e) => {
+        // Prevent default refreshing form on click
+        e.preventDefault();
+    };
+
+    const signIn = (e) =>{
+        // Prevent default refreshing form on click
+        e.preventDefault();
+    }
+
     return (
         <div className="signupScreen">
             <form>
                 <h1> Sign In</h1>
                 <input placeholder="Email" type="email"/>
                 <input placeholder="Password" type="password"/>
-                <button type="submit">Sign Up</button>
+                <button type="submit" onClick={signIn}>Sign In</button>
                 <h4>
                     <span className="signupScreen_gray">New to Netflix? </span> 
-                    <span className="signupScreen_link">Sign up now!</span>
+                    <span className="signupScreen_link" onClick={register}>Sign up now!</span>
                 </h4>
             </form>
         </div>
